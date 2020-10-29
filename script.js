c9831a3c93842647a4f1cf503bd2e26e7b8c04e5
Jay Valdez <jj.valdez09@gmail.com>
2020-10-29 17:36:28 -0500

    created a local storag to save any text to app

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ $(document).ready(function () {
     // display the current time and day
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 })
-    
+
     //creating a function with click listener and a local storage to set items
-    $(".saveBtn").on("click", function (){
+    $(".saveBtn").on("click", function () {
         console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
 
 })
+    // Creating a storage to load any saved data for each hour.
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
