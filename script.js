a858fd9c761a4a6b833caa15d597f316a1a14f04
Jay Valdez <jj.valdez09@gmail.com>
2021-03-24 22:01:17 -0500

    redoing this hw

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    // using moment.js display the current time and day
+
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //creating a function with click listener and a local storage to set items
+
     $(".saveBtn").on("click", function () {
         console.log(this);
         var text = $(this).siblings(".description").val();
@@ -10,25 +10,23 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
 
 })
-    // Creating a storage to load any saved data for each hour.
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    $("#Hr-9am .description").val(localStorage.getItem("9am"));
+    $("#Hr-10am .description").val(localStorage.getItem("10am"));
+    $("#Hr-11am .description").val(localStorage.getItem("11am"));
+    $("#Hr-12pm .description").val(localStorage.getItem("12pm"));
+    $("#Hr-1pm.description").val(localStorage.getItem("1pm"));
+    $("#Hr-2pm.description").val(localStorage.getItem("2pm"));
+    $("#Hr-3pm .description").val(localStorage.getItem("3pm"));
+    $("#Hr-4pm.description").val(localStorage.getItem("4pm"));
+    $("#Hr-5pm .description").val(localStorage.getItem("5pm"));
     
 
 
     function hourTracker(){
-        //using momemt.js to get current hours
         var currentHour = moment().hour();
-        
-        // loop for the time blocks and code to check if we past the time.
-        $(".time-block").each(function(){
+
+        $(".time-slots").each(function(){
             var blockHour = parseInt($(this).attr("id").split("hour")[1]);
             console.log(blockHour, currentHour)
 
